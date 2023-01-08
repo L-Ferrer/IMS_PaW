c6ca07be04fd18c48491ba88bac07c3db9aabcba
L-Ferrer <leandro.ferrer2005@gmail.com>
2023-01-08 22:19:53 +0100

    Added Navbar content

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -6,6 +6,7 @@ import democrats_crop from '/democrats_cropped.png'
 import republicans_crop from '/republicans_cropped.png'
 
 
+
 //TODO: https://www.framer.com/developers/guides/
 //TODO: https://mui.com/material-ui/react-typography/
 //TODO: Interactive map: https://blog.bitsrc.io/top-5-map-libraries-for-react-in-2021-20a37ff5234
@@ -15,6 +16,9 @@ function App() {
   return (
     <>
       <div className="navbar">
+        <a href="/" className='nav_child'>Home</a>
+        <a href="/map" className='nav_child'>Map</a>
+        <a href="/about" className='nav_child'>About</a>
       </div>
       <Background></Background>
       <div className="back">
