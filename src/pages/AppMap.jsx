d5185a2b13d67c40b1cb0d882ba710cae91154c5
L-Ferrer <leandro.ferrer2005@gmail.com>
2023-01-10 10:11:29 +0100

    Added embedded map

--- src/pages/AppMap.jsx
+++ src/pages/AppMap.jsx
@@ -3,20 +3,24 @@ import '../css/Map.css'
 import { Map, Marker, GeoJson } from "pigeon-maps"
 import data from '../assets/states_20m.json'
 
+const tmp = (
+    <div className="map_main">
+        <Map height={627} width={1536} defaultCenter={[38, -100]} defaultZoom={4.5}>
+            <Marker width={50} anchor={[38, -100]} />
+            <GeoJson
+                data={data}
+                styleCallback={(feature, hover) => {
+
+                }}
+            />
+        </Map>
+    </div>
+)
+
 function AppMap() {
     return (
         <>
-            <div className="map_main">
-                <Map height={627} width={1536} defaultCenter={[38, -100]} defaultZoom={4.5}>
-                    <Marker width={50} anchor={[38, -100]} />
-                    <GeoJson
-                        data={data}
-                        styleCallback={(feature, hover) => {
-                            
-                        }}
-                    />
-                </Map>
-            </div>
+            <a href="https://www.270towin.com/maps/2020-election-competitive-states"><img src="https://www.270towin.com/map-images/2020-election-competitive-states.png" width="800" /></a>
         </>
     )
 }
