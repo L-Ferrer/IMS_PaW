b8e11cf67e8297fade6b7647a0d87b8abe0a0e52
L-Ferrer <leandro.ferrer2005@gmail.com>
2023-01-10 08:53:08 +0100

    Fixed text based on 1080p monitor

--- src/comps/Content.jsx
+++ src/comps/Content.jsx
@@ -16,7 +16,6 @@ function Content() {
                                 entry.target.classList.add("show");
                             } else {
                                 entry.target.classList.remove("show");
-
                             }
                         }}
                     >
@@ -35,7 +34,7 @@ function Content() {
                             }
                         }}
                     >
-                        <Typography variant="h5" className="text">
+                        <Typography variant="h5" id="Republikaner_text" className="text">
                             Die Republikanische Partei, auch als Grand Old Party bekannt, wurde am 28. Februar 1854 gegründet und gilt in den USA als die 2. Grösste Partei. Sie hat sich ursprünglich gegen Sklaverei eingesetzt. Heute verfolgen sie vor allem konservative Politik. Ihre politische Richtung lässt sich durch folgende Punkte charakterisieren:
                             <br />
                             <ul>
@@ -95,7 +94,7 @@ function Content() {
                             }
                         }}
                     >
-                        <Typography variant="h5" className="text">
+                        <Typography variant="h5" id="BLM_text" className="text">
                             <p>BLM steht für Black Lives Matter. Diese internationale Gruppierung wurde 2013 nach dem Freispruch vom Polizisten, Goerge Zimmerman, für die Tötung von Trayvon Martin von den drei dunkelhäutigen Frauen, Alicia Garza, Patrisse Cullors und Opal Tometi gegründet.</p>
                             <p>BLM setzt sich für die Rechte von dunkelhäutigen Personen ein. Die Aktivisten nutzten anfänglich nur Social Media, um Anerkennung zu gewinnen doch mittlerweile protestieren sie öffentlich, marschieren oder sperren Strassen und Wege.</p>
                             <p>Die ursprüngliche Philosophie beschreibt eine der Gründerinnen wie folgt: «When we say Black Lies matter, we are talking about the ways in which black people are derpived of our basic human rights and dignity.[…]”.</p>
@@ -136,7 +135,7 @@ function Content() {
                             }
                         }}
                     >
-                        <Typography variant="h5" className="text">
+                        <Typography variant="h5" id="Feminismus_text" className="text">
                             <div>
                                 Der Begriff «Feminismus» entstand erst im späten 19. Jahrhundert. Er setzte sich in der internationalen Welle der neuen Bewegung ab 1968 weltweit als begriff durch.
                                 Grundsätzlich gibt es drei Fragen, die man sich stellen muss, um die Art von Feminismus erkennen zu können.
@@ -160,7 +159,7 @@ function Content() {
                             }
                         }}
                     >
-                        <Typography variant="h4" className={"subtitle"} id="Liberaler_Feminismus">Liberaler Feminismus</Typography>
+                        <Typography variant="h4" className="subtitle" id="Liberaler_Feminismus">Liberaler Feminismus</Typography>
                     </InView>
                     <InView
                         as="section"
@@ -175,7 +174,7 @@ function Content() {
                             }
                         }}
                     >
-                        <Typography variant="h5" className="text">
+                        <Typography variant="h5" id="Liberaler_Feminismus_text" className="text">
                             <div className="pblock">
                                 <p>Diese Art von Feminismus kritisiert die geschlechtliche Ungleichheit der Gesellschaft und der Demokratie und will die Gleichheit unabhängig vom Geschlecht in der bürgerlichen Gesellschaft erreichen.</p>
                                 <p>Diese Richtung hat kein ausgearbeitetes Genderkonzept, d.h. dass die Geschlechter von den mentalen Krankheiten eher eine persönliche Angelegenheit sind und sollten die Chancen im Beruf und in der Politik nicht beeinträchtigen.</p>
@@ -200,7 +199,7 @@ function Content() {
                             }
                         }}
                     >
-                        <Typography variant="h4" className={"subtitle"} id="Differenzfeminismus">Differenzfeminismus </Typography>
+                        <Typography variant="h4" className="subtitle" id="Differenzfeminismus">Differenzfeminismus </Typography>
                     </InView>
                     <InView
                         as="section"
@@ -215,7 +214,7 @@ function Content() {
                             }
                         }}
                     >
-                        <Typography variant="h5" className="text">
+                        <Typography variant="h5" id="Differenzfeminismus_text" className="text">
                             <div className="pblock">
                                 <p>Bei dieser Art von Feminismus ist vor allem die Betonung des Geschlechtsunterschieds im Zentrum. In Europa wurden sie von der romantischen Gegenbewegung zur Aufklärung inspiriert und beziehen sich auf die Gegenwerte zum «männlich zentriertem» Rationalismus.</p>
                                 <p>Aufgrund dieses theoretischen Geschlechtsunterschieds beanspruchen diese Feministen eigene wichtige Aufgabenbereiche der Frauen im modernen «Männerstaat». Die Frauen müssen als potenzielle Mütter für die Liebe, die Fürsorge und den Frieden stehen und dabei diese Werte in den «Männerstaat» einbringen.</p>
@@ -238,7 +237,7 @@ function Content() {
                             }
                         }}
                     >
-                        <Typography variant="h4" className={"subtitle"} id="Radikaler_Differenzfeminismus ">Radikaler Differenzfeminismus  </Typography>
+                        <Typography variant="h4" className="subtitle" id="Radikaler_Differenzfeminismus">Radikaler Differenzfeminismus  </Typography>
                     </InView>
                     <InView
                         as="section"
@@ -253,7 +252,7 @@ function Content() {
                             }
                         }}
                     >
-                        <Typography variant="h5" className="text">
+                        <Typography variant="h5" id="Radikaler_Differenzfeminismus_text" className="text">
                             <div className="pblock">
                                 <p>Diese Art von Feminismus legt das Patriarchat als System der Männerherrschaft über Frauen zugrunde. Diese Herrschaft wird von der «Sexualpolitik» bestimmt, wobei die Kontrolle der Sexualität und die Gewalt gegen Frauen aufrechterhalten wird.</p>
                                 <p>Mitglieder betonen die Geschlechterdifferenz und setzen auf weibliche Solidarität und Widerstand.</p>
@@ -275,7 +274,7 @@ function Content() {
                             }
                         }}
                     >
-                        <Typography variant="h4" className={"subtitle"} id="Konservativer_Feminismus">Konservativer Feminismus</Typography>
+                        <Typography variant="h4" className="subtitle" id="Konservativer_Feminismus">Konservativer Feminismus</Typography>
                     </InView>
                     <InView
                         as="section"
@@ -290,7 +289,7 @@ function Content() {
                             }
                         }}
                     >
-                        <Typography variant="h5" className="text">
+                        <Typography variant="h5" id="Konservativer_Feminismus_text" className="text">
                             <div className="pblock">
                                 <p>Auch diese Art von Feminismus geht eher von der Geschlechterdifferenz aus. Allerdings sieht er im Vergleich zu früher Frauen nicht als Mütter & Hausfrauen, sondern mehr für die Chancengleichheit im Beruf.</p>
                                 <p>Besonders Positionen im Management, in Medien und in der Politik werden dort gefördert. Dabei wird teilweise differenztheoretisch mit den weiblichen Fähigkeiten in Kommunikation und Beziehungen argumentiert, die vor allem in der Wirtschaft und der Politik von Vorteil sind.</p>
@@ -314,7 +313,7 @@ function Content() {
                             }
                         }}
                     >
-                        <Typography variant="h4" className={"subtitle"} id="Sozialistischer Feminismus">Sozialistischer Feminismus</Typography>
+                        <Typography variant="h4" className="subtitle" id="Sozialistischer_Feminismus">Sozialistischer Feminismus</Typography>
                     </InView>
                     <InView
                         as="section"
@@ -329,7 +328,7 @@ function Content() {
                             }
                         }}
                     >
-                        <Typography variant="h5" className="text">
+                        <Typography variant="h5" id="Sozialistischer_Feminismus_text" className="text">
                             <div className="pblock">
                                 <p>Diese Art von Feminismus legt in sein Geschlechterbild die Gleichheit ins Zentrum. Im Gesellschaftsbild werden die strukturellen Ungleichheiten nach Klasse, Geschlecht und Rasse kritisiert, besonders im Zusammenhang mit der kapitalistischen Gesellschaft.</p>
                                 <p>Diese Gesellschaft brachte den Ansatz der menschlichen Reproduktion, also der Geburt und Versorgung von Menschen in, dabei wird heute besonders nur noch die Versorgung der Menschen, und somit auch der gesellschaftlichen Arbeitskräfte, verstanden.</p>
@@ -352,7 +351,7 @@ function Content() {
                             }
                         }}
                     >
-                        <Typography variant="h4" className={"subtitle"} id="Transformativer_Feminismus">Transformativer Feminismus</Typography>
+                        <Typography variant="h4" className="subtitle" id="Transformativer_Feminismus">Transformativer Feminismus</Typography>
                     </InView>
                     <InView
                         as="section"
@@ -367,7 +366,7 @@ function Content() {
                             }
                         }}
                     >
-                        <Typography variant="h5" className="text">
+                        <Typography variant="h5" className="text" id="Transformativer_Feminismus_text">
                             <div className="pblock">
                                 <p>Beim transformativen Feminismus wird die Ungleichheit der Geschlechter in gesellschaftlichen Strukturen begründet, ähnlich wie beim sozialistischen Feminismus.</p>
                                 <p>Dabei wird der globale & neoliberale Kapitalismus aufgrund der sehr tiefen Ungleichheiten und die undemokratische Hinzufügung von Personen von sehr gebildeten und qualifizierten Frauen kritisiert.</p>
@@ -390,7 +389,7 @@ function Content() {
                             }
                         }}
                     >
-                        <Typography variant="h4" className={"subtitle"} id="Öko-Feminismus">Öko-Feminismus</Typography>
+                        <Typography variant="h4" className="subtitle" id="Öko-Feminismus">Öko-Feminismus</Typography>
                     </InView>
                     <InView
                         as="section"
@@ -405,7 +404,7 @@ function Content() {
                             }
                         }}
                     >
-                        <Typography variant="h5" className="text">
+                        <Typography variant="h5" id="Öko-Feminismus_text" className="text">
                             <div className="pblock">
                                 <p>Der ökologische Feminismus kritisiert unter anderem die Ausbeutung der Frauen, aber besonders speziell auch die Ausbeutung der Natur im patriarchalischen System des Kapitalismus.</p>
                                 <p>Während die älteren Strömungen besonders differenztheoretisch argumentieren, d.h. dass deren Argumente besonders im Unterschied zum Status quo basieren, während die neueren Strömungen eher auch die neuen Naturverhältnisse im Bezug zu den Geschlechtern hinterfragen.</p>
@@ -429,7 +428,7 @@ function Content() {
                             }
                         }}
                     >
-                        <Typography variant="h4" className={"subtitle"} id="Postkolonialer Feminismus">Postkolonialer Feminismus</Typography>
+                        <Typography variant="h4" className="subtitle" id="Postkolonialer_Feminismus">Postkolonialer Feminismus</Typography>
                     </InView>
                     <InView
                         as="section"
@@ -444,7 +443,7 @@ function Content() {
                             }
                         }}
                     >
-                        <Typography variant="h5" className="text">
+                        <Typography variant="h5" className="text" id="Postkolonialer_Feminismus_text">
                             <div className="pblock">
                                 <p>Bei dieser Richtung werden die Geschlechterverhältnisse besonders im Kontext der Machtverhältnisse der Welt kritisiert und möchten die Ungleichheiten der heutigen, postkolonialen Welt herausarbeiten.</p>
                                 <p>Die zwei Kritikpunkte, welche besonders oft genannt werden, ist der Unterschied von «weissen» Männern und Frauen zu den weiterhin untergeordneten «Subalternen» im Süden.</p>
@@ -484,7 +483,7 @@ function Content() {
                             }
                         }}
                     >
-                        <Typography variant="h5" className="text">
+                        <Typography variant="h5" className="text" id="Cancel_Culture_text">
                             <div>
                                 <p>Unter Cancel Culture versteht man seit Ende 2010s eine Art von Ausschluss eines Sozialen/Professionellem Umkreis einer Person. Dabei ist es irrelevant, ob dies Online, auf Sozialen Medien oder in Person geschieht.</p>
                                 <p>Ein weiterer Begriff, der mit Cancel Culture oft in Verbindung gebracht wird, ist Deplatforming.</p>
@@ -522,7 +521,7 @@ function Content() {
                             }
                         }}
                     >
-                        <Typography variant="h5" className="text">
+                        <Typography variant="h5" className="text" id="Me_Too_text">
                             <div>
                                 <p>#MeToo ist ein Hashtag in den sozialen Medien seit ungefähr Mitte Oktober 2017.</p>
                                 <p>Dieser Begriff lässt sich auf eine Aktivistin zurückführen und ermutigt Frauen mit Tweets auf das Ausmass von sexuellen Belästigungen und Übergriffen aufmerksam zu machen.</p>
@@ -530,6 +529,9 @@ function Content() {
                         </Typography>
                     </InView>
                 </Box>
+            </Container>
+            <Container className="container">
+
             </Container>
         </>
     )
