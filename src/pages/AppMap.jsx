2351d218e3239b63bfa1b4cb048325ce6f9004aa
L-Ferrer <leandro.ferrer2005@gmail.com>
2023-01-10 09:18:29 +0100

    Minor changes

--- src/pages/AppMap.jsx
+++ src/pages/AppMap.jsx
@@ -6,7 +6,7 @@ function AppMap() {
     return (
         <>
             <div className="map_main">
-                <Map height={500} width={1500} defaultCenter={[50.879, 4.6997]} defaultZoom={11}>
+                <Map height={627} width={1536} defaultCenter={[50.879, 4.6997]} defaultZoom={11}>
                     <Marker width={50} anchor={[50.879, 4.6997]} />
                 </Map>
             </div>
