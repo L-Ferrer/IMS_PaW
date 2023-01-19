7f5f34d447254fe088de92b904d387c92acb5f33
L-Ferrer <leandro.ferrer2005@gmail.com>
2023-01-19 04:44:34 +0100

    Finished map (2012-2020)

--- src/pages/AppMap.jsx
+++ src/pages/AppMap.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
+import { useState } from 'react';
 import '../css/Map.css'
-
 import { geoCentroid } from "d3-geo";
 import {
     ComposableMap,
@@ -9,25 +9,29 @@ import {
     Marker,
     Annotation
 } from "react-simple-maps";
-import allStates from "../assets/allstates.json";
-
-const geoUrl = "https://cdn.jsdelivr.net/npm/us-atlas@3/states-10m.json";
-
-const offsets = {
-    VT: [50, -8],
-    NH: [34, 2],
-    MA: [30, -1],
-    RI: [28, 2],
-    CT: [35, 10],
-    NJ: [34, 1],
-    DE: [33, 0],
-    MD: [47, 10],
-    DC: [49, 21]
-};
+import { Col, Row, Slider } from 'antd';
+import allStates_2020 from "../assets/allstates_2020.json";
+import allStates_2016 from "../assets/allStates_2016.json";
+import allStates_2012 from "../assets/allStates_2012.json";
 
 function AppMap() {
-    return (
-        <>
+    const geoUrl = "https://cdn.jsdelivr.net/npm/us-atlas@3/states-10m.json";
+    const offsets = {
+        VT: [50, -8],
+        NH: [34, 2],
+        MA: [30, -1],
+        RI: [28, 2],
+        CT: [35, 10],
+        NJ: [34, 1],
+        DE: [33, 0],
+        MD: [47, 10],
+        DC: [49, 21]
+    };
+    const [year, setYear] = useState("2020");
+
+    function VoteMap(states) {
+        const { allStates } = states;
+        return (
             <ComposableMap projection="geoAlbersUsa">
                 <Geographies geography={geoUrl}>
                     {({ geographies }) => (
@@ -46,19 +50,24 @@ function AppMap() {
                                     Object.keys(offsets).indexOf(cur.id) === -1 ? (
                                         <Geography
                                             key={geo.rsmKey}
-                                            stroke="#FFF"
+                                            stroke="#fff"
                                             geography={geo}
                                             fill={cur.color}
-                                        />
-                                    ) : (
-                                        <Annotation
-                                            subject={centroid}
-                                            dx={offsets[cur.id][0]}
-                                            dy={offsets[cur.id][1]}
+                                        />) : (
+                                        <Geography
+                                            key={geo.rsmKey}
+                                            stroke="#fff"
+                                            geography={geo}
+                                            fill={cur.color}
                                         >
-                                        </Annotation>
-                                    )
-                                )
+                                            <Annotation
+                                                subject={centroid}
+                                                dx={offsets[cur.id][0]}
+                                                dy={offsets[cur.id][1]}
+                                            >
+                                            </Annotation>
+                                        </Geography>
+                                    ))
                             })}
                             {geographies.map(geo => {
                                 const centroid = geoCentroid(geo);
@@ -73,8 +82,7 @@ function AppMap() {
                                                     <text y="2" fontSize={14} textAnchor="middle">
                                                         {cur.id}
                                                     </text>
-                                                </Marker>
-                                            ) : (
+                                                </Marker>) : (
                                                 <Annotation
                                                     subject={centroid}
                                                     dx={offsets[cur.id][0]}
@@ -86,12 +94,45 @@ function AppMap() {
                                                 </Annotation>
                                             ))}
                                     </g>
-                                );
+                                )
                             })}
                         </>
                     )}
                 </Geographies>
             </ComposableMap>
+        )
+    }
+
+    return (
+        <>
+            <div className='main'>
+                <h1>US Election Map {year}</h1>
+                {year === "2016" && <VoteMap allStates={allStates_2016} />}
+                {year === "2020" && <VoteMap allStates={allStates_2020} />}
+                {year === "2012" && <VoteMap allStates={allStates_2012} />}
+                <div className='legend'>
+                    <div className='blue'><p className='legend_text'> Democrats</p></div>
+                    <div className='red'><p className='legend_text'>Republicans</p></div>
+                    <div className='grey'><p className='legend_text'>Equal</p></div>
+                </div>
+                <div className='year_container'>
+                    <div className='year'>
+                        <Row style={{ justifyContent: "space-between" }}>
+                            <Col span={12}>
+                                <Slider
+                                    min={2012}
+                                    max={2020}
+                                    value={parseInt(year)}
+                                    step={4}
+                                    dots
+                                    onChange={(e) => { setYear(e.toString()); }}
+                                    style={{ width: "30vw" }}
+                                />
+                            </Col>
+                        </Row>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
